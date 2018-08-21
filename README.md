9fe60d9dd7c1340aae0459b206cc5967d0e0261b
raulanatol <raul@natol.es>
2018-08-21 19:02:17 +0100

    Update library

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This is a demo project to show the way to use the @nologis/maps component (https://github.com/Nologis/maps)
 
+## Plain html
+
+Using the nologis-map cdn:
+
+> plain-html/index.html
+
 ## Quick Overview
 
 ```bash
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { NologisMap } from '@nologis/maps';
+import { NologisDLMMap } from '@nologis/maps';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NologisMap/>
+        <NologisDLMMap apiKey='qa23AzyM2CbfhY3Y75mU1Zcps0uzyThr' venueId='f46b7922-310f-490d-bfe4-9bd6cc7eb79e'/>
       </div>
     );
   }
